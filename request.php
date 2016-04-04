ed26bffbf4c10f223d0af9852261f9078f2ea6c3
besmertn <besmertn@bk.ru>
2016-04-05 01:06:43 +0300

    fixed bugs

--- request.php
+++ request.php
@@ -10,41 +10,45 @@
     <?php
         include "dbConfig.php";
         $userName = $_SESSION['username'];
-        $query = "SELECT * FROM notes_".$userName;
-        echo $query;
-        $us = mysqli_query($dbcnx, $query);
-        if($us)
-        {
-            while($notes = mysqli_fetch_array($us, MYSQLI_ASSOC))
+        if($userName != "admin"){
+            $query = "SELECT * FROM notes_".$userName;
+            $us = mysqli_query($dbcnx, $query);
+            if($us)
             {
-              echo "<br>note id = ".$notes['note_id']."<br>";
-              echo "date = ".$notes['date']."<br>";
-              echo "title = ".$notes['title']."<br>";
-              echo "text = ".$notes['content']."<br>";
+                while($notes = mysqli_fetch_array($us, MYSQLI_ASSOC))
+                {
+                  echo "<br>note id = ".$notes['note_id']."<br>";
+                  echo "date = ".$notes['date']."<br>";
+                  echo "title = ".$notes['title']."<br>";
+                  echo "text = ".$notes['content']."<br>";
+                }
             }
-        }
-        else
-        {
-          echo "<p><b>Error: ".mysqli_error($dbcnx)."</b></p>";
-          exit();
-        }
-        /*$query = "SELECT * FROM users;";
-        $us = mysqli_query($dbcnx, $query);
-        if($us)
-        {
-            while($users = mysqli_fetch_array($us, MYSQLI_ASSOC))
+            else
             {
-              echo "<br>name = ".$users['name']."<br>";
-              echo "password = ".$users['password']."<br>";
-              echo "e-mail = ".$users['email']."<br>";
+              echo "<p><b>Error: ".mysqli_error($dbcnx)."</b></p>";
+              exit();
             }
-        }
-        else
-        {
-          echo "<p><b>Error: ".mysqli_error($dbcnx)."</b></p>";
-          exit();
-        }*/
-    mysqli_close($dbcnx);
+            /*$query = "SELECT * FROM users;";
+            $us = mysqli_query($dbcnx, $query);
+            if($us)
+            {
+                while($users = mysqli_fetch_array($us, MYSQLI_ASSOC))
+                {
+                  echo "<br>name = ".$users['name']."<br>";
+                  echo "password = ".$users['password']."<br>";
+                  echo "e-mail = ".$users['email']."<br>";
+                }
+            }
+            else
+            {
+              echo "<p><b>Error: ".mysqli_error($dbcnx)."</b></p>";
+              exit();
+            }*/
+        mysqli_close($dbcnx);
+    }
     ?>
+    <form>
+        <input type="button" onclick="document.location.href = 'main.php'" value="Back">
+    </form>
 </body>
 </html>
